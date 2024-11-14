2cbc32c347330ae23045a8f21142a6ded25ab59c
milenacarola <milenacarola19@gmail.com>
2024-11-14 18:09:36 -0400

    actualizacion1

--- apps.js
+++ apps.js
@@ -1,7 +1,20 @@
-const persona = {nombre: 'Boris', edad: 18, telefono: 65845123, avatar: '🧑🏻', bandera: true}
-console.log(persona);
+function sumar(a,b){
+    return a+b;
+}
+function restar(a,b){
+    return a-b;
+}
+function multiplicar(a,b){
+    return a*b
+}
+function dividir(a,b){
+    return a/b;
+}
 
-persona.edad = 25;
-console.log(persona);
+function calculadora(callback, num1, num2){
+    console.log(callback(num1,num2));
+}
+    
+calculadora(multiplicar,10,2);
 
-
+    
